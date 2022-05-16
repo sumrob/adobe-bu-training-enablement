c477a7cd246e8b59108268ea7a4a691db11bf55f
Robert Sumner <robert.sumner@perficient.com>
2022-05-16 13:28:55 -0500

    test amplify build

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React By Robert Sumner
         </a>
       </header>
     </div>
