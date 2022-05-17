d3f381e45b84de9477791ec5e313326cf0b939f5
Robert Sumner <robert.sumner@perficient.com>
2022-05-16 20:53:34 -0500

    intial ANT project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+/*!
+=========================================================
+* Muse Ant Design Dashboard - v1.0.0
+=========================================================
+* Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
+* Coded by Creative Tim
+=========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
+import Tables from "./pages/Tables";
+import Billing from "./pages/Billing";
+import Rtl from "./pages/Rtl";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Main from "./components/layout/Main";
+import "antd/dist/antd.css";
+import "./assets/styles/main.css";
+import "./assets/styles/responsive.css";
+import React, { Component }  from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React By Robert Sumner
-        </a>
-      </header>
+      <Switch>
+        <Route path="/sign-up" exact component={SignUp} />
+        <Route path="/sign-in" exact component={SignIn} />
+        <Main>
+          <Route exact path="/dashboard" component={Home} />
+          <Route exact path="/tables" component={Tables} />
+          <Route exact path="/billing" component={Billing} />
+          <Route exact path="/rtl" component={Rtl} />
+          <Route exact path="/profile" component={Profile} />
+          <Redirect from="*" to="/dashboard" />
+        </Main>
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
+/*!
+=========================================================
+* Muse Ant Design Dashboard - v1.0.0
+=========================================================
+* Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
+* Coded by Creative Tim
+=========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
